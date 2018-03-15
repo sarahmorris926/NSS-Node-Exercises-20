de351025539b6b9bb145f1b5334f003eed96da7a
Sarah Morris <sarah.morris9@gmail.com>
2018-03-15 10:56:07 -0500

    finished first requirement

--- express-hello-world.js
+++ express-hello-world.js
@@ -2,22 +2,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+// outputs HELLO WORLD when making GET request to root (/) url
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
-
-
-
-
-app.listen()
-
-
-// outputs HELLO WORLD when making GET request to root (/) url
-
 
 
 // outputs time stamp - new Date().toLocaleTimeString() - when making GET request to (/time) url
 
 
 // use env variable for port, use 8080 when one is not provided
-
+app.listen()
